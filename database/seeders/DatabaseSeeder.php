379a3f58b20bc5e4f7685ac827fcdd3b7ce8e4e4
Anggara Putra Pratama <75307272+anggaraputrapratama@users.noreply.github.com>
2024-02-09 11:26:07 +0700

    feat(auth): add login feature

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        /// membuat 10 data seed bisa diganti dengan jumlah data yang diingkan
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@pos.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+        ]);
     }
 }
---
+++ resources/views/pages/auth/login.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.auth')
+
+@section('title', 'Login')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
+@endpush
+
+@section('main')
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Login</h4>
+        </div>
+
+        <div class="card-body">
+            <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="email"
+                        class="form-control @error('email') is-invalid
+                    @enderror" name="email"
+                        tabindex="1" required autofocus>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+
+                <div class="form-group">
+                    <div class="d-block">
+                        <label for="password" class="control-label">Password</label>
+                        <div class="float-right">
+                            <a href="auth-forgot-password.html" class="text-small">
+                                Forgot Password?
+                            </a>
+                        </div>
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                        please fill in your password
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                        <span id="loginButtonText">Login</span>
+                        <span id="loginButtonLoader" class="d-none">
+                            <i class="fas fa-spinner fa-spin"></i> Loading...
+                        </span>
+                    </button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+    <div class="text-muted mt-5 text-center">
+        Don't have an account? <a href="auth-register.html">Create One</a>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+@endpush
+
+@push('scripts')
+    <!-- JS Libraries -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Page Specific JS File -->
+    <script>
+        $(document).ready(function() {
+            $('form').on('submit', function() {
+                $('#loginButton').prop('disabled', true); // Menonaktifkan tombol login
+                $('#loginButtonText').addClass('d-none'); // Menyembunyikan teks tombol
+                $('#loginButtonLoader').removeClass('d-none'); // Menampilkan ikon loading
+            });
+        });
+    </script>
+@endpush
